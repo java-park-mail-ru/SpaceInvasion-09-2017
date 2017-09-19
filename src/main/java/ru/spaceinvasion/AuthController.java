7d4dae8ff612b0556d34345ef50599a8bf3f93f6
Nikita-Boyarskikh <n02@yandex.ru>
2017-09-19 20:54:47 +0300

    Fix inspections

--- src/main/java/ru/spaceinvasion/AuthController.java
+++ src/main/java/ru/spaceinvasion/AuthController.java
@@ -3,6 +3,7 @@ package ru.spaceinvasion;
 import java.util.HashMap;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,61 +11,73 @@ import javax.servlet.http.HttpSession;
 
 
 @RestController
-@RequestMapping(path = "v1/auth")
+@RequestMapping(path = "v1/auth", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class AuthController {
 
-    @RequestMapping(method = RequestMethod.POST, path = "login", consumes = "application/json", produces = "application/json")
+    @RequestMapping(method = RequestMethod.POST, path = "login")
     public ResponseEntity<?> login(@RequestBody Auth auth, HttpSession httpSession) {
-        String login = auth.login;
-        String password = auth.password;
+        if ( !checkAuth(auth) ) {
+            return ResponseEntity.badRequest().build();
+        }
 
-        Auth curAuth = (Auth)httpSession.getAttribute("auth");
+        final Auth curAuth = (Auth)httpSession.getAttribute("auth");
         if ( curAuth != null ) {
             return ResponseEntity.badRequest().body(curAuth);
         }
 
         // TODO: Check if user exists in database
-        httpSession.setAttribute("auth", new Auth(login, password) );
+        httpSession.setAttribute("auth", auth );
 
-        return ResponseEntity.accepted().body("");
+        return ResponseEntity.accepted().build();
     }
 
-    @RequestMapping(method = RequestMethod.POST, path = "signup", consumes = "application/json", produces = "application/json")
+    @RequestMapping(method = RequestMethod.POST, path = "signup")
     public ResponseEntity<?> signUp(@RequestBody Auth auth, HttpSession httpSession) {
-        String login = auth.login;
-        String password = auth.password;
+        if ( !checkAuth(auth) ) {
+            return ResponseEntity.badRequest().build();
+        }
 
-        Auth curAuth = (Auth)httpSession.getAttribute("auth");
+        final Auth curAuth = (Auth)httpSession.getAttribute("auth");
         if ( curAuth != null ) {
             return ResponseEntity.badRequest().body(curAuth);
         }
 
-        httpSession.setAttribute("auth", new Auth(login, password) );
-        System.out.println(((Auth) httpSession.getAttribute("auth")).login);
+        httpSession.setAttribute("auth", auth );
         // TODO: Write new user into database
 
-        return ResponseEntity.accepted().body("");
+        return ResponseEntity.accepted().build();
     }
 
-    @RequestMapping(method = RequestMethod.POST, path = "logout", consumes = "application/json", produces = "application/json")
+    @RequestMapping(method = RequestMethod.POST, path = "logout", consumes = MediaType.ALL_VALUE)
     public ResponseEntity<?> logout( HttpSession httpSession ) {
         httpSession.invalidate();
         return ResponseEntity.accepted().body("");
     }
 
-    @RequestMapping(method = RequestMethod.GET, consumes = "application/json", produces = "application/json")
+    @RequestMapping(method = RequestMethod.GET, consumes = MediaType.ALL_VALUE)
     public ResponseEntity<?> curUser( HttpSession httpSession ) {
-        Auth curAuth = (Auth)httpSession.getAttribute("auth");
+        final Auth curAuth = (Auth)httpSession.getAttribute("auth");
 
         if (curAuth == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
 
-        HashMap hashMap = new HashMap<String, String>();
+        final HashMap<String, String> hashMap = new HashMap<>();
         hashMap.put("login", curAuth.login);
         hashMap.put("password", curAuth.password);
 
         return ResponseEntity.accepted().body(hashMap);
+    }
+
+    private boolean checkAuth(Auth auth) {
+        if ( auth == null ) {
+            return false;
+        }
+
+        final String login = auth.login;
+        final String password = auth.password;
+
+        return login != null && password != null;
     }
 }
 
