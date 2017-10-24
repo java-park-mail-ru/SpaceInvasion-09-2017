78c6b1ba96d242c89e6e15757ddecda6e1506aeb
Egor Kurakov <egor@live.ru>
2017-10-24 10:59:17 +0300

    Добавил Nullable аннотации

--- src/main/java/ru/spaceinvasion/models/User.java
+++ src/main/java/ru/spaceinvasion/models/User.java
@@ -4,16 +4,19 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.validator.constraints.NotBlank;
+import org.jetbrains.annotations.Nullable;
 
 import javax.validation.constraints.NotNull;
 
 public class User {
 
     @JsonIgnore
+    @Nullable
     private String password;
+    @Nullable
     private String username;
+    @Nullable
     private String email;
-    @JsonIgnore
     private int score;
     private int id;
 
