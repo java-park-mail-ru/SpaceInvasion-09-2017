d08a13e6befda13a169bc6a829328a06411a7ec4
Nikita-Boyarskikh <N02@yandex.ru>
2017-10-30 03:48:32 +0300

    Refactor origins into constants module and move
    hardcore from properies to environment variables

--- src/main/java/ru/spaceinvasion/utils/Constants.java
+++ src/main/java/ru/spaceinvasion/utils/Constants.java
@@ -7,4 +7,20 @@ public class Constants {
         public static final String LEADERBOARD_API_PATH = BASE_API_PATH + "/leaderboard";
         public static final int LEADERBOARD_SIZE = 10;
     }
+    public static class UrlConstants {
+        public static final String WEB_SOCKET_BASE_URL = "/game";
+        public static final String[] ALLOWED_ORIGINS = {
+                "*", // Для тестирования
+                        /* Продакш машина */
+                "http://space-invasion.ru",
+                "https://space-invasion.ru",
+                "http://www.space-invasion.ru",
+                "https://www.space-invasion.ru",
+                        /* Дев машина */
+                "http://space-invasion-frontend.herokuapp.com",
+                "https://space-invasion-frontend.herokuapp.com",
+                "http://www.space-invasion-frontend.herokuapp.com",
+                "https://www.space-invasion-frontend.herokuapp.com"
+        };
+    }
 }
