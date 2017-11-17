b12cf8949b74cddb037c2d79b565a37dd1d6d9b0
Egor Kurakov <egor@live.ru>
2017-11-18 01:13:28 +0300

    add config for websocket

---
+++ src/main/java/ru/spaceinvasion/WebSocketConfig.java
@@ -0,0 +1,31 @@
+package ru.spaceinvasion;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+import ru.spaceinvasion.utils.Constants;
+
+/**
+ * Created by egor on 18.11.17.
+ */
+
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer{
+
+    @NotNull
+    private final WebSocketHandler webSocketHandler;
+
+    public WebSocketConfig(@NotNull WebSocketHandler webSocketHandler) {
+        this.webSocketHandler = webSocketHandler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, Constants.UrlConstants.WEB_SOCKET_BASE_URL)
+                .addInterceptors(new HttpSessionHandshakeInterceptor())
+                .setAllowedOrigins("*");
+    }
+}
