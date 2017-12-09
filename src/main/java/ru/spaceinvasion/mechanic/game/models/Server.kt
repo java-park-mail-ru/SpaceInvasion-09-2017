5718bd5e4a08977cff45961b343a27f94c618df2
Egor Kurakov <egor@live.ru>
2017-12-09 23:30:34 +0300

    fix invalid request id returning

--- src/main/java/ru/spaceinvasion/mechanic/game/models/Server.kt
+++ src/main/java/ru/spaceinvasion/mechanic/game/models/Server.kt
@@ -20,8 +20,10 @@ class Server(mediator: GamePartMediator,
 
     var tickToCoinCreating: Int? = null
     val randomGenerator = RandomGenerator()
-    var playerPeopleLastProccesedSnapId : Long = 0
-    var playerAliensLastProcessedSnapId : Long = 0
+    var playerPeopleLastProccesedSnapId: Long = 0
+    var playerAliensLastProcessedSnapId: Long = 0
+    var playerPeopleId: Long = 0
+    var playerAliensId: Long = 0
 
     init {
         mediator.registerColleague(
@@ -35,38 +37,66 @@ class Server(mediator: GamePartMediator,
     }
 
     override fun notify(message: GameMessage) {
+        if (message.javaClass != RollbackMessage::class.java) {
+            commitRequest(message);
+        }
         when (message.javaClass) {
         //TODO: Maybe not message of this type put into snaps? Think about it and change it
             (RollbackMessage::class.java) -> {
-                snaps.filter { it.key == message.messageCreator.gamePartId * (-1) }.forEach { it.value.add(ServerSnap(message as RollbackMessage))}
+                snaps.filter { it.key == message.messageCreator.gamePartId * (-1) }.forEach {
+                    it.value.add(ServerSnap(message as RollbackMessage))
+                }
             }
             (MoveMessage::class.java) -> {
-                snaps.filter { it.key != (message.messageCreator as Unit).owner.gamePartId * (-1)  }.forEach { it.value.add(ServerSnap(message as MoveMessage))}
+                snaps.filter { it.key != (message.messageCreator as Unit).owner.gamePartId * (-1) }.forEach {
+                    modifyRequestId(it.key,message)
+                    it.value.add(ServerSnap(message as MoveMessage))
+                }
             }
             (BuildTowerMessage::class.java) -> {
-                snaps.filter { it.key != (message.messageCreator as Unit).owner.gamePartId * (-1)  }.forEach { it.value.add(ServerSnap(message as BuildTowerMessage))}
+                snaps.filter { it.key != (message.messageCreator as Unit).owner.gamePartId * (-1) }.forEach {
+                    modifyRequestId(it.key,message)
+                    it.value.add(ServerSnap(message as BuildTowerMessage))
+                }
             }
             (ShootMessage::class.java) -> {
-                snaps.filter { it.key != ((message.messageCreator as Shot).shotMaker as Unit).owner.gamePartId * (-1) }.forEach { it.value.add(ServerSnap(message as ShootMessage))}
+                snaps.filter { it.key != ((message.messageCreator as Shot).shotMaker as Unit).owner.gamePartId * (-1) }.forEach {
+                    modifyRequestId(it.key,message)
+                    it.value.add(ServerSnap(message as ShootMessage))
+                }
             }
             (DamageMessage::class.java) -> {
                 //MessageCreator reports about damage to him
                 //SrcOfDamage reports about guy who inflict damage
-                snaps.forEach { it.value.add(ServerSnap(message as DamageMessage)) }
+                snaps.forEach {
+                    modifyRequestId(it.key,message)
+                    it.value.add(ServerSnap(message as DamageMessage))
+                }
             }
             (BombInstallingMessage::class.java) -> {
-                snaps.forEach { it.value.add(ServerSnap(message as BombInstallingMessage)) }
+                snaps.forEach {
+                    modifyRequestId(it.key,message)
+                    it.value.add(ServerSnap(message as BombInstallingMessage))
+                }
             }
             (UnitCreationMessage::class.java) -> {
-                snaps.forEach { it.value.add(ServerSnap(message as UnitCreationMessage))}
+                snaps.forEach {
+                    modifyRequestId(it.key,message)
+                    it.value.add(ServerSnap(message as UnitCreationMessage))
+                }
             }
             (CoinAppearanceMessage::class.java) -> {
-                snaps.forEach { it.value.add(ServerSnap(message as CoinAppearanceMessage))}
+                snaps.forEach {
+                    modifyRequestId(it.key,message)
+                    it.value.add(ServerSnap(message as CoinAppearanceMessage))
+                }
             }
         }
     }
 
-    fun startGame(playerPeopleId : Long, playerAliensId : Long) {
+    fun startGame(playerPeopleId: Long, playerAliensId: Long) {
+        this.playerPeopleId = playerPeopleId
+        this.playerAliensId = playerAliensId
         snaps.put(playerPeopleId, ArrayList())
         snaps.put(playerAliensId, ArrayList())
         PlayerPeople(mediator, playerPeopleId * (-1), ID_GENERATOR)
@@ -79,11 +109,11 @@ class Server(mediator: GamePartMediator,
     }
 
     fun newClientTower(clientId: Long, snapId: Long, direction: Direction) {
-        mediator.send(BuildTowerMessage(this,snapId,direction), Player::class.java, clientId * (-1))
+        mediator.send(BuildTowerMessage(this, snapId, direction), Player::class.java, clientId * (-1))
     }
 
     fun newClientShot(clientId: Long, snapId: Long, direction: Direction) {
-        mediator.send(ShootMessage(this, snapId, direction),Player::class.java, clientId * (-1))
+        mediator.send(ShootMessage(this, snapId, direction), Player::class.java, clientId * (-1))
     }
 
     fun newClientStateRequest(clientId: Long, snapId: Long) {
@@ -95,20 +125,42 @@ class Server(mediator: GamePartMediator,
     }
 
     fun tick() {
-        mediator.send(TickMessage(this,0),Shot::class.java)
-        mediator.send(TickMessage(this,0),Tower::class.java)
-        mediator.send(TickMessage(this,0),Bomb::class.java)
-        mediator.send(TickMessage(this,0),Player::class.java)
+        mediator.send(TickMessage(this, 0), Shot::class.java)
+        mediator.send(TickMessage(this, 0), Tower::class.java)
+        mediator.send(TickMessage(this, 0), Bomb::class.java)
+        mediator.send(TickMessage(this, 0), Player::class.java)
         if (tickToCoinCreating != null) {
             tickToCoinCreating = tickToCoinCreating!! - 1
             if (tickToCoinCreating == 0) {
-                Coin(mediator,ID_GENERATOR.decrementAndGet(),ID_GENERATOR, randomGenerator.nextCoords())
+                Coin(mediator, ID_GENERATOR.decrementAndGet(), ID_GENERATOR, randomGenerator.nextCoords())
                 tickToCoinCreating = Constants.TICKS_TO_COIN_CREATING;
             }
 
         }
     }
 
+    private fun commitRequest(request: GameMessage) {
+        if (request.requestId == 0L) {
+            return
+        }
+        if (request.requestId > 0L && request.requestId % 2L == 1L) {
+            playerPeopleLastProccesedSnapId = request.requestId
+        }
+        if (request.requestId > 0L && request.requestId % 2L == 0L) {
+            playerAliensLastProcessedSnapId = request.requestId
+        }
+    }
+
+    private fun modifyRequestId(userId: Long, message: GameMessage) {
+        if (message.requestId != 0L) {
+            return
+        }
+        if (userId == minOf(playerAliensId, playerPeopleId)) {
+            message.requestId = playerPeopleLastProccesedSnapId
+        } else {
+            message.requestId = playerAliensLastProcessedSnapId
+        }
+    }
 
     class RandomGenerator() {
         val yMin: Int = Constants.Y_OF_UPPER_MAP_BORDER + Constants.VERTICAL_OFFSET_OF_COINS;
