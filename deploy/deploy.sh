12b5331d4aac0677b1e2fbd2620efb41c1253028
Nikita-Boyarskikh <N02@yandex.ru>
2017-12-16 14:15:38 +0300

    Fix paths

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh space_invasion@$BACKEND_HOST -o StrictHostKeyChecking=no 'cd /var/www/ && cd SpaceInvasion && sudo git checkout $TRAVIS_TAG && sudo git pull origin $TRAVIS_TAG && sudo docker rm -f $(sudo docker ps -aqf name=postgres) $(sudo docker ps -aqf name=backend); sudo docker-compose up --build'
+ssh space_invasion@$BACKEND_HOST -o StrictHostKeyChecking=no 'cd SpaceInvasionBackend && sudo git checkout $TRAVIS_TAG && sudo git pull origin $TRAVIS_TAG && sudo docker rm -f $(sudo docker ps -aqf name=postgres) $(sudo docker ps -aqf name=backend); sudo docker-compose up --build'
