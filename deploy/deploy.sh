67861249bba0fdbbe5e32099ee9b38c3dabf6675
Nikita-Boyarskikh <N02@yandex.ru>
2017-12-20 01:02:22 +0300

    add debug for deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh space_invasion@$BACKEND_HOST -o StrictHostKeyChecking=no 'cd SpaceInvasionBackend && sudo git checkout $TRAVIS_TAG && sudo git pull origin $TRAVIS_TAG && sudo docker rm -f $(sudo docker ps -aqf name=postgres) $(sudo docker ps -aqf name=backend); sudo docker-compose up --build'
+ssh space_invasion@$BACKEND_HOST -o StrictHostKeyChecking=no "echo $TRAVIS_TAG $BACKEND_HOST; pwd; ls -la; cd SpaceInvasionBackend && sudo git checkout $TRAVIS_TAG && sudo git pull origin $TRAVIS_TAG && sudo docker rm -f $(sudo docker ps -aqf name=postgres) $(sudo docker ps -aqf name=backend); sudo docker-compose up --build"
