f748669c07c462b2467624b9a9b384b78983a754
Nikita-Boyarskikh <N02@yandex.ru>
2017-12-20 01:23:32 +0300

    Add git fetch

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh space_invasion@$BACKEND_HOST -o StrictHostKeyChecking=no "cd SpaceInvasionBackend && sudo git checkout $TRAVIS_TAG && echo 1 && sudo git pull origin $TRAVIS_TAG && echo 2 && sudo docker rm -f $(sudo docker ps -aqf name=postgres) $(sudo docker ps -aqf name=backend); sudo docker-compose up --build"
+ssh space_invasion@$BACKEND_HOST -o StrictHostKeyChecking=no "cd SpaceInvasionBackend && sudo git fetch -t && sudo git checkout $TRAVIS_TAG && echo 1 && sudo git pull origin $TRAVIS_TAG && echo 2 && sudo docker rm -f $(sudo docker ps -aqf name=postgres) $(sudo docker ps -aqf name=backend); sudo docker-compose up --build"
